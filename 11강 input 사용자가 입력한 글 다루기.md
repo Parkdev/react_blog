5e01e3aefa59b2b414a328d2443cb54e5589dc12
devpark <devpark29@gmail.com>
2024-01-15 17:34:41 +0900

    11강 종료 숙제 완료

---
+++ 11강 input 사용자가 입력한 글 다루기.md
@@ -0,0 +1,22 @@
+## input 사용하기
+1. 종류가 여러개
+- text, textarea 등등
+2. 비슷한 내용으로 selectbox도 있다.
+3. 입력한 값 가져오기
+```html
+<input onChange={(e) => {함수}>
+```
+- e.target: 이벤트가 발생한 html 태그
+- e.target.value: 입력한 
+
+- 비고
+> 이벤트가 상위 요소로 퍼지는 현상: 이벤트 버블링
+     - 이걸 막고 싶으면
+```js
+(e)=> {
+    e.stopPropagation(); 이외 함수
+}
+```
+
+#### 숙제
+글 추가 삭제 만들어보기
--- blog/src/App.js
+++ blog/src/App.js
@@ -15,21 +15,21 @@ function App() {
   // let [글제목3, settitle3] = useState('겨울 코트 추천')
   // 이렇게 일일히 설정해도 되지만 array를 이용해서 하나의 변수로 사용가능하다
   let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동 맛집', '파이썬 독학']);
-  
   let [순번,순번변경] = useState(0);
-
   let [따봉, 따봉변경] = useState([0,0,0]);
-
   // 동적인 UI만들기
   let [modal, setModal] = useState(false);
+  // 인풋값 다루기
+  let [입력값, 입력값변경] = useState('');
+  
 
   //함수만들기
-  function 함수() {
-    console.log(1);
-  }
+  // function 함수() {
+  //   console.log(1);
+  // }
 
   //스타일 만들기
-  let btnstyle = { marginTop: '10px' }
+  // let btnstyle = { marginTop: '10px' }
   
   
 return(
@@ -72,11 +72,39 @@ return(
                   } }>👍</span> { 따봉[index] }
               </div>
               <p>2월 17일 발행</p>
+              <button className='mt-3 bg-gray-900 text-white rounded-md px-3 py-2 mr-3'
+                onClick={ () => {
+                  let copy = [...글제목];
+                  copy.shift();
+                  글제목변경(copy);
+                }}>삭제</button>
             </div>
           )
         })
       }
 
+        <input
+        type="text"
+        id="first_name"
+        className="mt-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5"
+        placeholder="제목 입력"
+        onChange={ (e) => {
+          //state 변경함수는 늦게 처리되는 함수이므로 로그 값이 우선 진행된다.
+          입력값변경(e.target.value);
+        }}/>
+        <button
+        type=""
+        className="ml-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm sm:w-auto px-5 py-2.5 text-center"
+        onClick={
+          () => {
+            let copy = [...글제목]
+            copy.unshift(입력값)
+            글제목변경(copy);
+          }
+        }
+        >업로드</button>
+
+      
       {/* <div className="list">
         <h4>{ 글제목[0] } <span onClick={ () => { 따봉변경(따봉+1) } }>👍</span> { 따봉 } </h4>
         <p>2월 17일 발행</p>
